b8c60da609fbf5fee51725289c193cc8c2cdc28a
Tarun K. Mall <c2tarun@hotmail.com>
2015-10-25 15:07:04 -0400

    Added GroupDetailActivity

---
+++ app/src/main/java/com/mad/metronome/ParseUtil.java
@@ -0,0 +1,11 @@
+package com.mad.metronome;
+
+/**
+ * Created by tarun on 10/25/15.
+ */
+public class ParseUtil {
+    public static void updateStrengthTable(String groupId, int strength) {
+        
+
+    }
+}
